102084f17bf3e48a734974edf4f0a7f3777bc874
Vishnuhan <vishnu1456@hotmail.com>
2024-04-05 17:40:37 -0400

    for render deployment

--- backend/server.js
+++ backend/server.js
@@ -509,8 +509,7 @@ app.use('/auth', notiRoutes);
 app.use('/auth', tempRoutes);
 
 
-// Listen on all network interfaces
-const PORT = 3001;
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
